4049751a2dac314b71ac54a9ef7efeaaa7d641b2
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2017-10-13 16:24:39 +0900

    show more detail

--- StripeAPISample/Views/ProductDetail/ProductDetailViewController.swift
+++ StripeAPISample/Views/ProductDetail/ProductDetailViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-final class ProductDetailViewController: UITableViewController, Storyboardable {
+final class ProductDetailViewController: UIViewController, Storyboardable {
     var product: StripeAPI.Entity.Product!
 
     @IBOutlet weak var productNameLabel: UILabel!
     @IBOutlet weak var captionLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
 
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +25,45 @@ final class ProductDetailViewController: UITableViewController, Storyboardable {
         productNameLabel.text = product.name
         captionLabel.text = product.caption
         descriptionLabel.text = product.description
+        idLabel.text = product.id
 
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+
+extension ProductDetailViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath)
+    }
+}
+
+extension ProductDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "SKUs"
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        let sku = product.skus.data[indexPath.row]
+        cell.textLabel?.text = sku.attributes.description
+        switch sku.inventory.type {
+        case .bucket:
+            if let value = sku.inventory.value{
+                cell.detailTextLabel?.text = "Bucket: \(value)"
+            }
+        case .finite:
+            if let quantity = sku.inventory.quantity{
+                cell.detailTextLabel?.text = "在庫数: \(quantity)"
+            }
+        case .infinite:
+            cell.detailTextLabel?.text = "在庫無限"
+        }
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return product.skus.data.count
     }
 }
