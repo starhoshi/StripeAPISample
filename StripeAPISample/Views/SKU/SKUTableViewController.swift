bc3993a240b4ddc2da3877a8fdd8bb2fb974da73
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2017-10-16 04:55:42 +0900

    implement STPContextDelegate

--- StripeAPISample/Views/SKU/SKUTableViewController.swift
+++ StripeAPISample/Views/SKU/SKUTableViewController.swift
@@ -12,6 +12,7 @@ import Stripe
 final class SKUTableViewController: UITableViewController, Storyboardable {
     var product: StripeAPI.Entity.Product!
     var sku: StripeAPI.Entity.SKU!
+    var paymentContext: STPPaymentContext!
 
     @IBOutlet weak var skuLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
@@ -27,6 +28,63 @@ final class SKUTableViewController: UITableViewController, Storyboardable {
         priceLabel.text = "\(sku.price)円"
 
         let customerContext = STPCustomerContext(keyProvider: StripeCustomerKeyAPI.shared)
+        let paymentContext = STPPaymentContext(customerContext: customerContext,
+                                               configuration: STPPaymentConfiguration.shared(),
+                                               theme: .default())
+        let userInformation = STPUserInformation()
+        paymentContext.prefilledInformation = userInformation
+        paymentContext.paymentAmount = sku.price
+        paymentContext.paymentCurrency = sku.currency
+        self.paymentContext = paymentContext
+    }
+}
 
+// MARK: STPPaymentContextDelegate
+
+extension SKUTableViewController: STPPaymentContextDelegate {
+    func paymentContext(_ paymentContext: STPPaymentContext, didFailToLoadWithError error: Error) {
+        let alertController = UIAlertController(
+            title: "Error",
+            message: error.localizedDescription,
+            preferredStyle: .alert
+        )
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: { action in
+            // Need to assign to _ because optional binding loses @discardableResult value
+            // https://bugs.swift.org/browse/SR-1681
+            _ = self.navigationController?.popViewController(animated: true)
+        })
+        let retry = UIAlertAction(title: "Retry", style: .default, handler: { action in
+            self.paymentContext.retryLoading()
+        })
+        alertController.addAction(cancel)
+        alertController.addAction(retry)
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+    func paymentContextDidChange(_ paymentContext: STPPaymentContext) {
+        print(paymentContext)
+    }
+
+    func paymentContext(_ paymentContext: STPPaymentContext, didFinishWith status: STPPaymentStatus, error: Error?) {
+        let title: String
+        let message: String
+        switch status {
+        case .error:
+            title = "Error"
+            message = error?.localizedDescription ?? ""
+        case .success:
+            title = "Success"
+            message = "You bought a \(self.product)!"
+        case .userCancellation:
+            return
+        }
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+
+    func paymentContext(_ paymentContext: STPPaymentContext, didCreatePaymentResult paymentResult: STPPaymentResult, completion: @escaping STPErrorBlock) {
+        completion(nil) // サーバでの決済が完了した
     }
 }
