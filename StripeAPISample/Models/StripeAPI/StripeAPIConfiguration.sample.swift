a413789b26a16d0ff01638787dac66e3df80e96e
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2017-10-16 14:35:29 +0900

    update sample configuration

--- StripeAPISample/Models/StripeAPI/StripeAPIConfiguration.sample.swift
+++ StripeAPISample/Models/StripeAPI/StripeAPIConfiguration.sample.swift
@@ -10,15 +10,13 @@ import Foundation
 
 // rename to StripeAPIConfiguration and set parameters
 class SampleStripeAPIConfiguration {
-    static let shared = SampleStripeAPIConfiguration()
-    private init () { }
+    /// https://dashboard.stripe.com/account/apikeys
+    static let publishableKey = "pk_test_xxxxxx"
 
     /// https://dashboard.stripe.com/account/apikeys
-    var publishableKey = ""
-
-    /// https://dashboard.stripe.com/account/apikeys
-    var secretKey = ""
+    static let secretKey = "sk_test_xxxxxxxx"
 
     /// https://github.com/stripe/example-ios-backend/tree/v11.0.0
-    var customerKeyURL = URL(string: "https://your-example.heroku.com")
+    static let customerKeyURL = URL(string: "https://hoge.com")!
 }
+
